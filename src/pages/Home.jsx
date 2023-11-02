0c5d572cd79b06e8ca5ffd57f98b40bcd8b1e722
Joelle  Johnson <joellej221@gmail.com>
2023-11-02 14:50:27 -0400

    creating skeleton for routing practice. Basics of routing in react are displayed in App.js

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+export default function Home() {
+    return (
+        <>
+            <h2>Home</h2>
+        </>
+    )
+}
